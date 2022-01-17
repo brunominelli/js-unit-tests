79b2fe365806b610ab116a7053cc2c72bdacf643
Bruno Minelli <minellibruno@hotmail.com>
2022-01-17 15:59:15 -0300

    Requisito 03 finalizado.

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  let text = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  
+  if (!name || !age) text = undefined;
+  return text;
+};
 
 module.exports = vqv;
